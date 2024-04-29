9b6469b7adef203c62e90ee6dc51bf326cc75c1d
Jyrone Parker <jyrone.parker@gmail.com>
2024-04-29 01:22:58 -0400

    Update local-run.sh

--- local-run.sh
+++ local-run.sh
@@ -1,2 +1,2 @@
 echo "RUNNING AND WAITING FOR CONNECTIONS"
-python3.11 http_server.py --certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem --verbose -q .  --host ::
+python3.11 examples/http3_server.py --certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem --verbose -q .  --host ::
